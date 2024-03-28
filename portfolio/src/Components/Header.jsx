1ce73cedb5c3e482a1f44f2bf7779b8cad748b21
Monde Nel <Mondenel1996@gmail.com>
2024-03-28 13:13:08 +0200

    added styling to the navbar

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Home, Work, Mail } from '@mui/icons-material';
+import styles from './Header.module.css'; 
 
 function Header() {
   return (
-    <header>
-      <h1>My Portfolio</h1>
-      <nav>
-        <ul>
-          <li><Link to="/">About</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <AppBar position="static" className={styles.header}>
+      <Toolbar>
+        <Typography variant="h6" component="div" className={styles.title}>
+          My Portfolio
+        </Typography>
+        <nav>
+          <ul className={styles.navbar}>
+            <li>
+              <IconButton component={Link} to="/" color="inherit">
+                <Home />
+              </IconButton>
+            </li>
+            <li>
+              <IconButton component={Link} to="/projects" color="inherit">
+                <Work />
+              </IconButton>
+            </li>
+            <li>
+              <IconButton component={Link} to="/contact" color="inherit">
+                <Mail />
+              </IconButton>
+            </li>
+          </ul>
+        </nav>
+      </Toolbar>
+    </AppBar>
   );
 }
 
