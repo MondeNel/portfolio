bd107f79808a4c2792eb9d2475d5f3d0fa46bdce
Monde Nel <Mondenel1996@gmail.com>
2024-03-30 23:28:22 +0200

    created the about component and also added styling to it

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import About from './Components/About';
 
 
 const App = () => {
   return (
    <div>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <About />
    </div>
   );
 }
