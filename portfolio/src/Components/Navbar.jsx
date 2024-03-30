2f281d19e0deb7452a40b1f089ad845ee807e879
Monde Nel <Mondenel1996@gmail.com>
2024-03-30 22:00:20 +0200

    added the icons and styled them for the list on the side of the screen

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -3,6 +3,9 @@ import logo from '../assets/mbblogo.jpg'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import ArticleIcon from '@mui/icons-material/Article';
 
 const Navbar = () => {
 
@@ -44,12 +47,33 @@ const Navbar = () => {
 
 
         {/* social icon */}
-        <div className='flex fixed flex-col top-[35%] left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a className='flex justify-between items-center w-full text-gray-300'
+                    <a className='flex justify-between items-center w-full text-white'
                     href="/">
                         LinkedIn <LinkedInIcon size={30}/>
+                    </a>
+                    </li>
+
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                    <a className='flex justify-between items-center w-full text-white'
+                    href="/">
+                        GitHub <GitHubIcon size={30}/>
+                    </a>
+                    </li>
+
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6de4ca]'>
+                    <a className='flex justify-between items-center w-full text-white'
+                    href="/">
+                        Email <EmailIcon size={30}/>
+                    </a>
+                    </li>
+
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#5f5f5f]'>
+                    <a className='flex justify-between items-center w-full text-white'
+                    href="/">
+                        Resume <ArticleIcon size={30}/>
                     </a>
                     </li>
                 </ul> 
