36ea11a806e7d4844f6c32fd611372d401772965
Monde Nel <Mondenel1996@gmail.com>
2024-04-02 13:37:23 +0200

    added the Link scroll in the Home component to connect to the Workk

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
@@ -11,15 +12,18 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Monde Nel</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Software Developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>As a junior level software developer, I bring key skills to a team, including
-                JavaScript, React, HTML, CSS. I ahve a strong problem-solving ability, and I'm known
+                JavaScript, React, HTML, CSS. I have a strong problem-solving ability, and I'm known
                 for my excellent communication skills, which facilitate effective communication.
             </p>
             <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-800 hover:border-blue-800'>View work 
-                    <span className='group-hover:rotate-90 duration-300'>
-                        <ArrowForwardIcon className='ml-3'/>
-                    </span>
-               </button>
+                <Link to='work' smooth={true} duration={500}>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-800 hover:border-blue-800'>
+                        View work 
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <ArrowForwardIcon className='ml-3'/>
+                        </span>
+                    </button>
+                </Link>
             </div>
          </div>
 
@@ -28,4 +32,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
