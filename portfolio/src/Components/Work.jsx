8f1961832022bb50e51d44281f21428ffcef4ed3
Monde Nel <Mondenel1996@gmail.com>
2024-04-02 11:51:51 +0200

    added the right styling for the grid layout and the containers are positioned accordingly for the projects

--- portfolio/src/Components/Work.jsx
+++ portfolio/src/Components/Work.jsx
@@ -7,41 +7,36 @@ import weather from '../assets/Work/weather1.png'
 const Work = () => {
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#120c2c]'>
+
+
+       {/* conatiner */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
+            <div>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-800'>Work</p>
-                <p className='py-4'>// check out my recent projects</p>
+                <p className='py-4'>// Check projects</p>
             </div>
 
             {/* grid container */}
-            <div className='grid sm:grid-col-2 md:grid-col-3 gap-4'>
-
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
                 {/* grid item 1 */}
-                <div style={{ backgroundImage: `url(${bookConnect})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        {/* Hover effect */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                BookConnect Application
-                            </span>
-
-                            <div className='pt-8 text-center'>
-                                <a href="https://bookbrowser.netlify.app/" target="_blank" rel="noopener noreferrer">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href="https://github.com/MondeNel/Bookbrowser_application" target="_blank" rel="noopener noreferrer">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-
-
-                            </div>
+                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' style={{ backgroundImage: `url(${bookConnect})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '280px' }}>
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>BookConnect Application</span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://bookbrowser.netlify.app/" target="_blank" rel="noopener noreferrer">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="https://github.com/MondeNel/Bookbrowser_application" target="_blank" rel="noopener noreferrer">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
                         </div>
+                    </div>
+                </div>
                  
 
                     {/* grid item 2 */}
-                    <div style={{ backgroundImage: `url(${quiz})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' style={{ backgroundImage: `url(${quiz})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '280px' }}>
 
                         {/* Hover effect */}
                         <div className='opacity-0 group-hover:opacity-100'>
@@ -62,7 +57,7 @@ const Work = () => {
                     </div>
 
                       {/* grid item 3 */}
-                      <div style={{ backgroundImage: `url(${movie})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                      <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' style={{ backgroundImage: `url(${movie})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '280px' }}>
 
                             {/* Hover effect */}
                             <div className='opacity-0 group-hover:opacity-100'>
@@ -82,7 +77,7 @@ const Work = () => {
                         </div>
 
                         {/* grid item 4 */}
-                      <div style={{ backgroundImage: `url(${weather})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' style={{ backgroundImage: `url(${weather})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '280px' }}>
 
                             {/* Hover effect */}
                             <div className='opacity-0 group-hover:opacity-100'>
@@ -101,7 +96,6 @@ const Work = () => {
                             </div>
                         </div>
             </div>
-
             
         </div>
     </div>
