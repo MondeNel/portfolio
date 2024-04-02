191bd6bb7f729c46daa33f129ba1608dffeb71e7
Monde Nel <Mondenel1996@gmail.com>
2024-04-02 13:08:28 +0200

    added a button and also styling to the text field area

--- portfolio/src/Components/Contact.jsx
+++ portfolio/src/Components/Contact.jsx
@@ -6,7 +6,12 @@ const Contact = () => {
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-800 text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'>// Submit the form below or send me an email - Mondenel1996@gmail.com</p>
             </div>
+            <input type="text" placeholder='Name' name='name' className='bg-[#ccd6f6] p-2'/>
+            <input type="text" placeholder='Email' name='email' className='my-4 p-2 bg-[#ccd6f6]'/>
+            <textarea name="message" rows="10" placeholder='Message' className='bg-[#ccd6f6] p-2'></textarea>
+            <button className='text-white border-2 hover:bg-blue-600 hover:border-blue-600 px-4 p-3 my-8 mx-auto flex items-center'>Let's collaborate</button>
         </form>
     </div>
   )
